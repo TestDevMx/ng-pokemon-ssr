8e13e6a3ffecdc7d03e201c080f22a67893395c6
testdevmx <test.dev.mx@gmail.com>
2024-10-20 21:35:44 -0600

    initial commit

---
+++ src/app/pages/about-page/about-page.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './about-page.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export default class AboutPageComponent {
+
+}
---
+++ src/app/pages/pricing-page/pricing-page.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pricing-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './pricing-page.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export default class PricingPageComponent {
+
+}
