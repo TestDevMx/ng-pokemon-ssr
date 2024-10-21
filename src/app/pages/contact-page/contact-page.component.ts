215cea717ce9dbc418801732b6b6c3566f78eb2f
testdevmx <test.dev.mx@gmail.com>
2024-10-20 22:26:28 -0600

    ssr pages

--- src/app/pages/contact-page/contact-page.component.ts
+++ src/app/pages/contact-page/contact-page.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, type OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-contact-page',
@@ -7,6 +8,16 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './contact-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export default class ContactPageComponent {
+export default class ContactPageComponent implements OnInit {
+
+  private title = inject(Title);
+  private meta = inject(Meta);
+
+  ngOnInit(): void {
+    this.title.setTitle('Contact page');
+    this.meta.updateTag({ name: 'description', content: 'Este es mi Contact Page' });
+    this.meta.updateTag({ name: 'og:title', content: 'Contact page' });
+    this.meta.updateTag({ name: 'keywords', content: 'Hola,Mundo,testdev,angular,PRO' });
+  }
 
 }
--- src/app/pages/pricing-page/pricing-page.component.ts
+++ src/app/pages/pricing-page/pricing-page.component.ts
@@ -1,4 +1,6 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { isPlatformBrowser, isPlatformServer } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject, PLATFORM_ID, type OnInit } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-pricing-page',
@@ -7,6 +9,22 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './pricing-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export default class PricingPageComponent {
+export default class PricingPageComponent implements OnInit {
+
+  private title = inject(Title);
+  private meta = inject(Meta);
+  private platform = inject(PLATFORM_ID);
+  ngOnInit(): void {
+    // console.log({ platform: this.platform });
+
+    // if (isPlatformBrowser(this.platform)) {
+    //   document.title = 'Pricing page';
+    // }
+
+    this.title.setTitle('Pricing page');
+    this.meta.updateTag({ name: 'description', content: 'Este es mi Pricing Page' });
+    this.meta.updateTag({ name: 'og:title', content: 'Pricing page' });
+    this.meta.updateTag({ name: 'keywords', content: 'Hola,Mundo,testdev,angular,PRO' });
+  }
 
 }
